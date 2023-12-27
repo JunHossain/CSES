6224fb339901ff98c15f166b01689a8d9a169846
Jun Hossain <junaidhossain45@gmail.com>
2023-12-27 16:35:34 +0600

    dead

--- CSES/TowerOfHanoi.cpp
+++ CSES/TowerOfHanoi.cpp
@@ -1,7 +1,22 @@
-// The code is not yet finished
+// https://www.youtube.com/watch?v=rf6uf3jNjbo
+// I understand each and everything except the
+// recursion visualization :"(
 
 #include <bits/stdc++.h>
 using namespace std;
+
+void hanoi(int numberofDisks, int start, int end)
+{
+    if (numberofDisks == 1)
+        cout << start << " " << end << endl;
+    else
+    {
+        int middle = 6 - (start + end);
+        hanoi(numberofDisks - 1, start, middle);
+        cout << start << " " << end << endl;
+        hanoi(numberofDisks - 1, middle, end);
+    }
+}
 
 int optimalSolution(int numberofDisks)
 {
@@ -13,4 +28,5 @@ int main()
     int numberofDisks;
     cin >> numberofDisks;
     cout << optimalSolution(numberofDisks) << endl;
+    hanoi(numberofDisks, 1, 3); // start = 1, end = 3
 }
