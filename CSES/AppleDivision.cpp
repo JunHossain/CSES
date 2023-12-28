fcc9844b97a1b1471ba5815855f8a470549e7d49
Jun Hossain <junaidhossain45@gmail.com>
2023-12-28 20:13:53 +0600

    dead

--- CSES/AppleDivision.cpp
+++ CSES/AppleDivision.cpp
@@ -1,27 +1,51 @@
-// This code is unfinished
-
 #include <bits/stdc++.h>
 using namespace std;
+
+void subsets(int arr[], int n, long long totalSum)
+{
+    long long int difference = LONG_LONG_MAX;
+    long long int minimumDifference = LONG_LONG_MAX;
+    for (int i = 0; i < (1 << n); i++)
+    {
+        long long int subsetSum = 0;
+        for (int j = 0; j < n; j++)
+        {
+            if (i & (1 << j))
+            {
+                subsetSum += arr[j];
+            }
+        }
+        long long secondSubsetSum = abs(totalSum - subsetSum);
+
+        difference = min(difference, abs(subsetSum - secondSubsetSum));
+
+        if (difference < minimumDifference)
+        {
+            minimumDifference = difference;
+        }
+    }
+
+    cout << minimumDifference << endl;
+}
 
 int main()
 {
     int n;
     cin >> n;
     int arr[n];
-    long long int sum = 0;
+    long long int totalSum = 0;
 
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
-        sum += arr[i];
+        totalSum += arr[i];
     }
 
-    if (sum % 2 != 0)
+    if (n == 1)
     {
-        cout << "1" << endl;
+        cout << arr[0] << endl;
+        return 0;
     }
-    else
-    {
-        cout << "0" << endl;
-    }
+
+    subsets(arr, n, totalSum);
 }
