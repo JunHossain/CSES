dc9e5b3d3ad4a4dd6bedf593aa46cf90f65b7073
Jun Hossain <junaidhossain45@gmail.com>
2024-01-04 21:26:16 +0600

    dead

---
+++ Vjudge/KhelaHobe.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+
+    if (y > 6 * x || y < x)
+        cout << "NO" << endl;
+    else
+        cout << "YES" << endl;
+}
